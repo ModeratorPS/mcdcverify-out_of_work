1eb6524c7d2ca65e7d0295fde9cb91a57fe1d431
ModeratorPS <102473254+ModeratorPS@users.noreply.github.com>
2022-07-23 12:13:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,32 +6,31 @@ const config = require('./config.json');
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const { error } = require('console');
+const hostn = config['mc-bot-ip'];
 
 const bot = mineflayer.createBot({
-    host: `${config['mc-server-ip']}`, // minecraft server ip
-    username: `${config['mc-bot-username']}`, // minecraft username
-    // password: '12345678' // minecraft password, comment out if you want to log into online-mode=false servers
-    //port: `00000`,                // only set if you need a port that isn't 25565
-    // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-    // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
+    host: `${hostn}`,
+    username: `${config['mc-bot-username']}`,
+    //port: `51553`,                // only set if you need a port that isn't 25565
 });
   
 bot.on('chat', (username, message) => {
     if (message.startsWith("!verify")) {
-        let args = message.split(" ") //.slice(3).join(" ").split(",")
+        let args = message.split(" ")
         if (!args[1]) return bot.chat("Usage: !verify <Discord user id (Find out your user id with !id on Discord!)>")
         bot.chat(`Search ${args[1]}`)
         const successembed = new MessageEmbed()
             .setTitle("Verify-System")
             .setColor('14b82d')
             .setDescription('You have successfully linked your Minecraft account to Discord!')
-        const errorembed = new MessageEmbed() //comming soon
-            .setTitle("Verify-System")
-            .setColor('b31919')
-            .setDescription('You have already linked your Minecraft account to Discord!')
+        const unlinkbutton = new MessageActionRow()
+            .addComponents(
+            new MessageButton()
+                .setStyle("DANGER")
+                .setCustomId("unlink")
+                .setLabel("Unlink")
+            )
         client.guilds.fetch(`${config['dc-server-guild-id']}`).then(guild => {
-            //const user = guild.members.cache.find(u => u.tag === `${args[1]}`)
-            //if(!user) return bot.chat(`User ${args[1]} could not be found`);
             client.users.fetch(`${args[1]}`).then(user => {
                 client.guilds.fetch(`${config['dc-server-guild-id']}`).then(guild => {
                     let roleName2 = `${config['dc-server-role-name']}`;
@@ -40,7 +39,8 @@ bot.on('chat', (username, message) => {
                     guild.members.cache.filter(n => n.user.id = `${args[1]}`).forEach(member => member.roles.add(requirerole))
                 })
                 user.send({
-                    embeds: [successembed]
+                    embeds: [successembed],
+                    components: [unlinkbutton]
                 })
                 bot.chat(`${args[1]} verifyed!`)
             }).catch(err => {
@@ -50,6 +50,28 @@ bot.on('chat', (username, message) => {
         })
     }
 });
+
+client.on('interactionCreate', interaction => {
+    const unverifyembed = new MessageEmbed()
+        .setTitle("Verify-System")
+        .setColor('b31919')
+        .setDescription('You have unlinked your Minecraft account to Discord!')
+    client.guilds.fetch(`${config['dc-server-guild-id']}`).then(guild => {
+        client.users.fetch(`${interaction.user.id}`).then(user => {
+            client.guilds.fetch(`${config['dc-server-guild-id']}`).then(guild => {
+                let roleName2 = `${config['dc-server-role-name']}`;
+                const role2 = guild.roles.cache.find(x2 => x2.name === roleName2);
+                const requirerole = role2.id
+                guild.members.cache.filter(n => n.user.id = `${interaction.user.id}`).forEach(member => member.roles.remove(requirerole))
+                user.send({
+                    embeds: [unverifyembed]
+                })
+            })
+        }).catch(err => {
+            console.error(err)
+        })
+    })
+})
 
 client.once('ready', () => {
     console.log('The Client is Ready!')
